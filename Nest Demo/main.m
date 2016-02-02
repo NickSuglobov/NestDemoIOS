f2987fbde6fe069a3aab8211ee0ab0903089b1e6
Nikolay Suglobov <msuhlobau@scnsoft.com>
2016-02-02 19:22:07 +0300

    Initial Commit

---
+++ Nest Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Nest Demo
+//
+//  Created by Nikolay Suglobov on 2/2/16.
+//  Copyright © 2016 Appko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
